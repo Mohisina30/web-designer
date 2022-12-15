821a7e5f98140f2c2f27210da4a085f601fa8ebb
Mohisina30 <mohisinakhatoon30@gmail.com>
2022-12-15 10:10:39 +0530

    js

---
+++ js/arithmetic.js
@@ -0,0 +1,8 @@
+function add(){
+    let x;// creating a new variable x
+    x=6;//assigning a 6 value to x
+    let y;// creating a new variable y
+    y=1;//assigning a 17 value to y
+    let z=x + y;// creating a new vaariable z and addition is done
+    document.getElementById('add').innerHTML=z;
+}
---
+++ js/index.js
@@ -0,0 +1,10 @@
+function resume(){
+    let firstname;
+    firstname =document.getElementById("firstname").Value;
+    console.log(firstname);
+        let lastname;
+        lastname=document.getElementById("lastname").Value;
+    console.log(lastname);
+    document.getElementById("name").innerHTML=firstname+ lastname;
+      
+}
